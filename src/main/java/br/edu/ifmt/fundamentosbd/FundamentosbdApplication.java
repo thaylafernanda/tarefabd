7b4cad7da375672eed5cbd38433a1f8a4fec3317
thaylafernanda <geraldo.neto@plc.ifmt.edu.br>
2021-09-03 10:55:17 -0400

    Adicionando arquivos do Spring Boot

---
+++ src/main/java/br/edu/ifmt/fundamentosbd/FundamentosbdApplication.java
@@ -0,0 +1,13 @@
+package br.edu.ifmt.fundamentosbd;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FundamentosbdApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FundamentosbdApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/edu/ifmt/fundamentosbd/FundamentosbdApplicationTests.java
@@ -0,0 +1,13 @@
+package br.edu.ifmt.fundamentosbd;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FundamentosbdApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
